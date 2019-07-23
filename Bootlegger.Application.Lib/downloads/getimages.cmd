2e01d01b6a918a9e9fff03d9a80a7853e179513f
Tom Bartindale <tom@bartindale.com>
2019-07-23 12:40:59 +1000

    update pipeline

--- Bootlegger.Application.Lib/downloads/getimages.cmd
+++ Bootlegger.Application.Lib/downloads/getimages.cmd
@@ -7,6 +7,4 @@ docker pull bootlegger/ourstory-worker:dev
 docker pull bootlegger/ourstory-server:dev 
 docker pull bootlegger/nginx-local:latest
 echo "Exporting Images"
-docker export redis:alpine mvertes/alpine-mongo kusmierz/beanstalkd bootlegger/nginx-local:latest bootlegger/ourstory-worker:dev bootlegger/ourstory-server:dev -o images.tar
-rem docker save bootlegger/ourstory-worker:latest -o images2.tar
-rem docker save bootlegger/ourstory-server:latest -o images3.tar
+docker save redis:alpine mvertes/alpine-mongo kusmierz/beanstalkd bootlegger/nginx-local:latest bootlegger/ourstory-worker:dev bootlegger/ourstory-server:dev -o images.tar
