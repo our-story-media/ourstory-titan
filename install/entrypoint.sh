9c5ac27556f271b3c961bf45cc147c1347cf5e7a
Tom Bartindale <tom.bartindale@monash.edu>
2019-08-16 15:09:02 +1000

    entrypoint, building for multiple arch compact

--- install/entrypoint.sh
+++ install/entrypoint.sh
@@ -1,71 +1,48 @@
 #!/bin/bash
 echo "Starting Our Story Titan"
 
-mongod &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start mongod: $status"
-  exit $status
-fi
+APPS=("mongod" "redis-server --dir /redis --appendonly yes" "beanstalkd" "nginx -g \"daemon off;\"")
 
-# Start the second process
-redis-server --dir /redis --appendonly yes &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start redis-server: $status"
-  exit $status
-fi
-
-# Start the second process
-beanstalkd &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start beanstalkd: $status"
-  exit $status
-fi
-
-sleep 5
-
-# Start the second process
-nginx -g "daemon off;" &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start nginx: $status"
-  exit $status
-fi
-
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container exits with an error
-# if it detects that either of the processes has exited.
-# Otherwise it loops forever, waking up every 60 seconds
-
-while sleep 60; do
-  ps aux |grep mongod |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep redis-server |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  ps aux |grep beanstalkd |grep -q -v grep
-  PROCESS_3_STATUS=$?
-  ps aux |grep nginx |grep -q -v grep
-  PROCESS_4_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0  -o $PROCESS_3_STATUS -ne 0 -o $PROCESS_4_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit 1
-  fi
+for ((i = 0; i < ${#APPS[@]}; i++))
+do
+  echo "Starting ${APPS[$i]}"
+  eval "${APPS[$i]}" &
 done
 
-
-
-# mongod &
-# redis-server --dir /redis --appendonly yes &
-# beanstalkd &
-
-# sleep 2
+sleep 5
 
 cd /ourstory-worker && npm start &
 cd /ourstory-server && npm start &
 
-# nginx -g "daemon off;"
+while sleep 10; do
+  #check node apps:
+  # echo "Checking worker"
+  ps aux |grep "node index.js" |grep -q -v grep
+  STATUS=$?
+  if [ $STATUS -ne 0 ] ; then
+    echo "Worker Not Running!"
+    exit 1
+  fi
+
+  # echo "Checking server"
+  ps aux |grep "node app.js --prod" |grep -q -v grep
+  STATUS=$?
+  if [ $STATUS -ne 0 ] ; then
+    echo "Server Not Running!"
+    exit 1
+  fi
+
+  #check deps:
+  for ((i = 0; i < ${#APPS[@]}; i++))
+  do
+    set -- ${APPS[$i]}
+    APP=$1
+    # echo "Checking $APP"
+    ps aux |grep $APP |grep -q -v grep
+    STATUS=$?
+    if [ $STATUS -ne 0 ] ; then
+      echo "$APP Not Running!"
+      exit 1
+    fi
+  done
+done
