7285da8528ff5d4c34487f116a963871f9133417
Tom Bartindale <tom.bartindale@monash.edu>
2019-08-22 09:12:47 +1000

    redirect to 8845 rather than 80 (to maintain consistency with installed)

--- install/build-compact.sh
+++ install/build-compact.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building Compact Titan Image"
 # ,linux/amd64,linux/arm64, linux/arm/v7
-docker buildx build --add-host=redis:127.0.0.1,mongo:127.0.0.1,beanstalk:127.0.0.1,web:127.0.0.1 --platform=linux/arm/v7 -t bootlegger/titan-compact --push .
+docker buildx build --no-cache --add-host=redis:127.0.0.1,mongo:127.0.0.1,beanstalk:127.0.0.1,web:127.0.0.1 --platform=linux/arm/v7 -t bootlegger/titan-compact --push .
